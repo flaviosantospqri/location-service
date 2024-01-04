ce77120ab61c24e066b2cb47f96cf67c7270ac6c
Flavio Santos <flavio.pasantos@outlook.com>
2024-01-04 18:01:42 -0300

    feat: inserted feature in signin page

--- src/pages/SignInPage/index.jsx
+++ src/pages/SignInPage/index.jsx
@@ -1,10 +1,10 @@
 import signinImg from "../../img/signin.svg";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../../components/OAuth";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { db } from "../../services/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -12,6 +12,8 @@ const SignIn = () => {
     password: "",
   });
   const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
 
   const { email, password } = form;
 
@@ -25,19 +27,32 @@ const SignIn = () => {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCreadential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCreadential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Ocurred some error");
+    }
+  };
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">SignIn</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
-          <img
-            src={signinImg}
-            alt="Sign Image"
-            className="w-full rounded-2xl"
-          />
+          <img src={signinImg} alt="Sign" className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mt-5"
               type="text"
