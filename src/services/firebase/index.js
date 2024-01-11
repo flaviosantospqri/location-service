5d809c8acdeeb2cf2c9ff0833d608d6b6f4d8b14
Flavio Santos <flavio.pasantos@outlook.com>
2024-01-11 10:32:56 -0300

    fix: security adjusts

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -5,13 +5,22 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBJLMg5YCDmH1Kus1luMagMu9_-g4o0QU4",
-  authDomain: "location-app-6d63b.firebaseapp.com",
-  projectId: "location-app-6d63b",
-  storageBucket: "location-app-6d63b.appspot.com",
-  messagingSenderId: "217882888957",
-  appId: "1:217882888957:web:4cb83eeaebf520aeb28c94",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
